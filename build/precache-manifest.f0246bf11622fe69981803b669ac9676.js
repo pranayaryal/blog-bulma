28718fc50e6fb5505c39fedd935c338fc01821b4
pranayaryal <drpranayaryal@gmail.com>
2019-01-05 14:16:54 -0500

    adding build folder

---
+++ build/precache-manifest.f0246bf11622fe69981803b669ac9676.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "5dc94ad65ff4960a7b6f602639fb3c94",
+    "url": "/static/media/mountain-hero.5dc94ad6.jpg"
+  },
+  {
+    "revision": "986770584ab2a3e3141b",
+    "url": "/static/css/main.f109b749.chunk.css"
+  },
+  {
+    "revision": "c8e872bb9567649e25c1",
+    "url": "/static/js/1.c8e872bb.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cbc48e9bd5a1f424f108e10476bfb200",
+    "url": "/static/media/me.cbc48e9b.jpg"
+  },
+  {
+    "revision": "986770584ab2a3e3141b",
+    "url": "/static/js/main.98677058.chunk.js"
+  },
+  {
+    "revision": "b596dd87a84c7320e1ca83cc85bdf41c",
+    "url": "/static/media/book-svg.b596dd87.svg"
+  },
+  {
+    "revision": "098adfac762a21e7da55bcf78af1a022",
+    "url": "/static/media/cobblestone.098adfac.jpg"
+  },
+  {
+    "revision": "7434e26fd970d56de758ee75d5585577",
+    "url": "/static/media/pink-mountain.7434e26f.jpg"
+  },
+  {
+    "revision": "f36792cedd7426897715c84c9969859a",
+    "url": "/static/media/antique-backdrop-background-733853.f36792ce.jpg"
+  },
+  {
+    "revision": "dbf5cf3cc1752582fd8511a7257d7168",
+    "url": "/index.html"
+  }
+];
